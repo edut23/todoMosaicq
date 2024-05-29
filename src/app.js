ea33ecb9c258f93be4af13ada5eedbac82ef7edf
Edu Teodoro <edu.teodoro@tebesensor.com>
2024-05-29 10:59:53 -0300

    runAgain

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config()
 // ==> Rotas da API:
 const index = require('./routes/index');
 const itemRoute = require('./routes/item.routes');
