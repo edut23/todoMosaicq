9ce6bd1e46e6c34a76a335369681056459bb5973
Edu Teodoro <edu.teodoro@tebesensor.com>
2024-05-29 09:46:41 -0300

    vercelIntegration

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require('./src/app');
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('Aplicação executando na porta ', port);
+app.listen(() => {
+  console.log('Aplicação executando');
 });
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 // ==> Conexão com a Base de Dados:
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.POSTGRES_URL
 });
 pool.on('connect', () => {
   console.log('Base de Dados conectado com sucesso!');
