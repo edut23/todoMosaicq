2fd2a40c5d00f8ef1533c83878d3e9f6c56ce6ec
Edu Teodoro <edu.teodoro@tebesensor.com>
2024-05-29 10:06:30 -0300

    updateEnv

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require('./src/app');
-app.listen(() => {
-  console.log('Aplicação executando');
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log('Aplicação executando na porta ', port);
 });
